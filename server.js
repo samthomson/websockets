6ec71da957e80f34b3027349e3c02c3063ee1ce2
Sam Thomson <samt@samt.st>
2018-12-29 13:08:08 +0700

    simplify

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// set up server
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+// serve a client
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// define websocket interfaces
 io.on('connection', function(socket){
   console.log('a user connected');
 });
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
